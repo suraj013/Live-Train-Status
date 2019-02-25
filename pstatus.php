c6ccfeca69496f1fb12612458d40254fbd0a3ef7
SK GOLAM DASTIGIR <41388308+suraj013@users.noreply.github.com>
2019-02-25 19:03:50 +0530

    Update pstatus.php

--- pstatus.php
+++ pstatus.php
@@ -4,15 +4,32 @@
 	$p_no = $_GET['p_no'];
 	echo "PNR Number : ".$p_no."<br>";
 	
-	$url = "https://api.railwayapi.com/v2/pnr-status/pnr/".$p_no."/apikey/ho4rrofgtu/";
+	$url = "https://api.railwayapi.com/v2/pnr-status/pnr/".$p_no."/apikey/9goopts6gm/";
 	$data = file_get_contents($url);
 	$json = json_decode($data,true);
-	echo "<br> Train Name : ".$json['train']['name'];
-	echo "<br> Train Number : ".$json['train']['number'];
-	echo "<br> Date Of Journey : ".$json['doj'];
-	echo "<br> Total Passengers : ".$json['total_passengers'];
-	echo "<br> Journey Class : ".$json['journey_class']['code'];
-	echo "<br> Boarding Point: ".$json['boarding_point']['name'];
-	echo "<br> Reserved Upto: ".$json['reservation_upto']['name'];
+	$tna = "<br> Train Name : ".$json['train']['name'];
+	echo $tna;
+	$tnu = "<br> Train Number : ".$json['train']['number'];
+	echo $tnu;
+	$doj = "<br> Date Of Journey : ".$json['doj'];
+	echo $doj;
+	$tp = "<br> Total Passengers : ".$json['total_passengers'];
+	echo $tp;
+	$jc = "<br> Journey Class : ".$json['journey_class']['code'];
+	echo $jc;
+	$bp = "<br> Boarding Point: ".$json['boarding_point']['name'];
+	echo $bp;
+	$ru = "<br> Reserved Upto: ".$json['reservation_upto']['name'];
+	echo $ru;
 
+	$con = mysqli_connect('localhost','root');
+	if(!$con)
+		echo "Connection Failed";
+	else
+		echo "Connection Done";
+	mysqli_select_db($con,'db2');
+	$q="insert into pnr (pid,pnr,tname,tno,doj,tp,jc,bp,ru) values($pid, $p_no, '$tname', '$tno', '$doj' '$tp' '$jc' '$bp' '$ru')";
+	mysqli_query($con,$q);
+
+	mysqli_close($con);
 ?>
