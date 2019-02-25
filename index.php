6c7bf20436fd3d71990130ad373c8c413bc82c9b
SK GOLAM DASTIGIR <41388308+suraj013@users.noreply.github.com>
2019-02-25 18:59:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 
 	<title>Railway Service</title>
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <style>
 a:link, a:visited {
@@ -12,6 +11,7 @@ a:link, a:visited {
     color: white;
     padding: 14px 25px;
     text-align: center;
+    font-weight: 1000;
     text-decoration: none;
     display: inline-block;
     padding: 20px;
@@ -19,31 +19,53 @@ a:link, a:visited {
     text-shadow: 6px 6px 12px red;
 }
 
-
 a:hover, a:active {
     background-color: violet;
     padding: 20px;
     margin: 50px 50px 50px 175px;
     text-shadow: 6px 6px 12px red;
-
+}
+button:hover {
+    background-color: violet;
+    padding: 20px;
+    text-shadow: 6px 6px 12px red;
+}
+button{
+    background-color: #CD9973;
+    color: white;
+    padding: 14px 25px;
+    text-align: center;
+    font-weight: 1000;
+    text-decoration: none;
+    display: inline-block;
+    padding: 15px;
+    margin: 50px 25px 50px 520px;
+    text-shadow: 6px 6px 12px red;
+}
+p{
+    font-weight: 1000;
+    margin: 0px 25px 10px 450px;
+}
 </style>
-
-<button type="button"
-onclick="document.getElementById('time').innerHTML = Date()">
-Click here to display Date and Time.</button>
-<p id="time"></p>
-
+<script>
+    function f1()
+    {
+        var x=document.getElementById("time").innerHTML = Date();
+    }
+</script>
 </head>
 <body>
-	<marquee><h1 id="hh1">WELCOME TO INDIAN RAILWAYS<h1></marquee>
-	<h1 id="hh2">TRACK YOUR RAIL</h1>
+<marquee><h1 id="hh1">WELCOME TO INDIAN RAILWAYS<h1></marquee>
 
-	<a href="https://indiarailinfo.com/" target="_blank">Search Your Train</a>
-		<a href="train.php" target="_blank">Check TRAIN Status</a>
-				<a href="pnr.php" target="_blank">Check PNR Status</a>
+<button type="button" onclick="f1()"> Click here to display Date and Time.</button>
 
-
-
+<p id="time"></p>
 	
+	<h1 id="hh2">TRACK YOUR RAIL INFORMATION</h1>
+
+	<a href="station.php" target="_blank">Search Your Train</a>
+	<a href="train.php" target="_blank">Check TRAIN Status</a>
+	<a href="pnr.php" target="_blank">Check PNR Status</a>
+
 </body>
 </html>
