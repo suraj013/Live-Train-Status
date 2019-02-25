8a979c41e06b02cc58391e9404167eb7b18624ed
SK GOLAM DASTIGIR <41388308+suraj013@users.noreply.github.com>
2019-02-25 19:03:06 +0530

    Update pnr.php

--- pnr.php
+++ pnr.php
@@ -4,21 +4,51 @@
 	
 	<title>PNR Status</title>
 	<link rel="stylesheet" type="text/css" href="mystyle.css">
-
-<button type="button"
-onclick="document.getElementById('time').innerHTML = Date()">
-Click here to display Date and Time.</button>
-<p id="time"></p>
-
-	
+<style>
+button{
+    background-color: #CD9973;
+    color: white;
+    padding: 14px 25px;
+    text-align: center;
+    font-weight: 1000;
+    text-decoration: none;
+    display: inline-block;
+    padding: 15px;
+    margin: 20px 0px 20px 520px;
+    text-shadow: 6px 6px 12px red;
+}
+button:hover {
+    background-color: pink;
+    padding: 20px;
+    text-shadow: 6px 6px 12px red;
+    }
+p{
+    font-weight: 1000;
+    margin: 0px 25px 10px 450px;
+}
+strong
+{
+	margin: 10px 0px 10px 480px;
+	font-size: 40px;
+	color:rgb(65,7,7);
+}
+</style>
+<script>
+    function f1()
+    {
+        var x=document.getElementById("time").innerHTML = Date();
+    }
+</script>
 </head>
 <body>
 	<marquee><h1 id="hh1">WELCOME TO INDIAN RAILWAYS<h1></marquee>
-	<h1 id="hh2">CHECK PNR STATUS</h1>
-
+	<button type="button" onclick="f1()"> Click here to display Date and Time.</button>
+	<p id="time"></p>
+    <h1 id="hh2">CHECK PNR STATUS</h1>
 		<form method="get" action="pstatus.php">
-			<strong>Enter the PNR Number :<strong> <input type = "text" name = "p_no"/><br>
-			<button type = "submit"> CHECK </button>
+			<strong>Enter the PNR Number :<strong> <br>
+			<input style="margin: 20px 0px 20px 100px;" type = "text" placeholder="Your PNR" name = "p_no"/><br>
+			<button style="margin: 20px 0px 20px 625px;background-color: #674EDA;" type = "submit"> CHECK </button>
 		</form>
 </body>
 </html>
