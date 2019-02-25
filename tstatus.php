5f35d91183ea21b49b46fa2a485f5fe0890b1ee2
SK GOLAM DASTIGIR <41388308+suraj013@users.noreply.github.com>
2019-02-25 19:06:49 +0530

    Update tstatus.php

--- tstatus.php
+++ tstatus.php
@@ -3,13 +3,33 @@
 <?php
 	$t_no = $_GET['t_no'];
 	$date = $_GET['date'];
+	$sn = $_GET['sn'];
+	
+	//MySql Connection
+	$con = mysqli_connect('localhost','root');
+	if(!$con)
+		echo "Connection Failed<br>";
+	else
+		echo "Connection Done<br>";
+	mysqli_select_db($con,'rail');
+	$u="Select CODE from stations where name like '".$sn."%'";
+	$res=mysqli_query($con,$u);
+	$num=mysqli_num_rows($res);
+	for($i=1;$i<=$num;$i++)
+	{
+		$w=mysqli_fetch_array($res);
+		$v= $w['CODE'];
+	}
+	mysqli_close($con);
+	//MySql Connection Ends
+
 	echo "Train Number : ".$t_no."<br>";
 	$y = substr($date, 0,4);
 	$m = substr($date, 5,2);
 	$d = substr($date, 8,2);
 	$nd= $d."-".$m."-".$y;
 	echo "Date : ".$nd."<br>";
-	$url = "https://api.railwayapi.com/v2/live/train/".$t_no."/date/".$nd."/apikey/ho4rrofgtu/";
+	$url = "https://api.railwayapi.com/v2/live/train/".$t_no."/station/".$v."/date/".$nd."/apikey/9goopts6gm/";
 	$data = file_get_contents($url);
 	$json = json_decode($data,true);
 	echo "<br> Train Name : ".$json['train']['name'];
